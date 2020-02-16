d84e22b8c81017ec354a02317a7273efd4542362
RG <rgosens2@gmailcom>
2020-02-17 00:56:50 +0100

    Test8

--- hello_metal_6dof/Renderer.m
+++ hello_metal_6dof/Renderer.m
@@ -21,7 +21,7 @@ static const NSUInteger kMaxBuffersInFlight = 3;
 
 static const size_t kAlignedUniformsSize = (sizeof(Uniforms) & ~0xFF) + 0x100;
 
-// Git test7
+// Git test8
 
 
 @implementation Renderer
